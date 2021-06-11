29fa800725152e5ff6e3128dedf99469c347612f
Colin Nicol <9090461+cnicol@users.noreply.github.com>
2021-06-11 08:24:54 -0700

    First attempt at adding in volumes from dams

--- HUC_12_contrib_areas_2021_06_04.R
+++ HUC_12_contrib_areas_2021_06_04.R
@@ -1,26 +1,34 @@
-#Instal packages
-install.packages("dplyr")
-install.packages("purrr")
+#Install packages
 library(purrr)
 
 # Read dbf file in shapefile
-#df <- foreign::read.dbf("C://01_Projects/NWFSC_misc/Britta/HUC_12_SkagitWater_boundary_correct/HUC_12_SkagitWater_boundary_correct.dbf")
-df <- read.csv ("D:/R/SkagitWaterSupply_Demand/data/input.csv")
+
+df <- read.csv ("data/input/Flow_Accumulation_28.03.2021.csv")
 df$HUC12 <- as.character(df$HUC12)
 
 # Get list of HUCs
 all_hucs <- unique(df$HUC12)
 
 
+# Replace data with data from dams
+huc_w_dams <- c('171100050708', '171100051007')
+df[df$HUC12 %in% huc_w_dams, 'Total_Med_Vol'] <- c(3091988, 1906057)
+
 
 # Return contributing sheds for a given HUC
 find_contrib_HUCs <- function(HUC_code){
-  df[df$TOHUC == HUC_code, 'HUC12']
+
+  if(HUC_code %in% huc_w_dams){
+    NULL
+  } else {
+    df[df$TOHUC == HUC_code, 'HUC12']
+  }
+
 }
 
 
+
 # Iteratively search for contributing HUCs
-#find_contrib_area <- function(df, HUC_code){
 find_contrib_vol <- function(df, HUC_code){
   x <- HUC_code
   y <- x
@@ -31,7 +39,6 @@ find_contrib_vol <- function(df, HUC_code){
   }
 
   data.frame(HUC12 = HUC_code,
-             #Total_area = sum(df[df$HUC12 %in% x, 'AREASQKM']),
              Total_Accum_Vol = sum(df[df$HUC12 %in% x, 'Total_Med_Vol']),
              contrib_HUCs_count = length(x),
              contrib_HUCs = paste(x, collapse = '-')
@@ -39,9 +46,7 @@ find_contrib_vol <- function(df, HUC_code){
 }
 
 
-#df_out <- purrr::map_dfr(all_hucs, ~find_contrib_area(df, .x))
 df_out <- purrr::map_dfr(all_hucs, ~find_contrib_vol(df, .x))
 
 
-write.csv(df_out,'D:/R/SkagitWaterSupply_Demand/data/output/HUC_12_contrib_Volumes.csv')
-
+write.csv(df_out,'data/HUC_12_contrib_Volumes.csv')
